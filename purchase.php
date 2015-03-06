de9428abd23c26ffa7279f16445bc8ba42acadef
Masaki Kagaya <masakielastic@gmail.com>
2015-03-06 17:17:40 +0900

    fixed the usage of json_encode

--- purchase.php
+++ purchase.php
@@ -3,9 +3,9 @@ require_once 'config.php';
 require_once 'functions.php';
 
 if (empty($_POST['amount']) || empty($_POST['token'])) {
-    header('Content-Type: application/json', true, 400);
-    echo json_encode(array('status' => '400'));
-    exit();
+  header('Content-Type: application/json', true, 400);
+  echo json_encode(array('status' => '400'));
+  exit();
 }
 
 $data = array(
@@ -16,10 +16,11 @@ $data = array(
 
 $ret = webpay_charges($private_key, $data);
 
-$json_options = 0;
 if (version_compare(PHP_VERSION, '5.3.0') >= 0) {
-    $json_options |= JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT;
+  $json = json_encode($ret, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT);
+} else {
+  $json = json_encode($ret);
 }
 
 header('Content-Type: application/json', true, 201);
-echo json_encode($ret, $json_options);
+echo $json;
