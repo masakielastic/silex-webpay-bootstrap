7b097634e966d832ef5a3795bde77b3ad64879df
Masaki Kagaya <masakielastic@gmail.com>
2015-03-22 07:04:05 +0900

    adopted Bootstrap

--- js/example.js
+++ js/example.js
@@ -1,34 +1,47 @@
 $(function() {
+
   $.ajaxSetup({
     beforeSend: function(xhr) {
       xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
     }
   });
 
-  var el = $('#paymentForm input[type=submit]');
+  var msg = $('#alertMsg');
+  var el = $('#paymentSubmit');
 
   el.click(function(event) {
     event.preventDefault();
     el.prop('disabled', true);
 
-    var formData = $('#paymentForm').serializeArray();
+    var formData = {
+      'amount': amount = $('#inputAmount').val(),
+      'webpay-token': $('#paymentForm').serializeArray()[0]['value']
+    };
 
-    if (formData[0]['value'] === '') {
-      $('#msg').html('金額を入力してください。');
+    if (formData['amount'] === '') {
+      msg.children('p').html('金額を入力してください。');
+      msg.show();
       el.prop('disabled', false);
       return false;
     }
 
-    if (formData[1]['value'] === '') {
-      $('#msg').html('カード情報を入力してください。');
+    if (formData['webpay-token'] === '') {
+      msg.children('p').html('カード情報を入力してください。');
+      msg.show();
       el.prop('disabled', false);
       return false;
     }
 
     $.post(uri, formData).done(function(data, textStatus, jqXHR) {
-      $('#msg').html('ありがとうございました。');
+      msg.removeClass('alert-danger');
+      msg.addClass('alert-success');
+      msg.children('p').html('ありがとうございます。');
+      msg.show();
+
+      el.removeClass('btn-primary');
+      el.addClass('btn-success');
     }).fail(function(jqXHR, textStatus, errorThrown) {
-      $('#msg').html('投稿が失敗しました。' + jqXHR.responseJSON['msg']);
+      msg.children('p').html('投稿が失敗しました。' + jqXHR.responseJSON['msg']);
       el.prop('disabled', false);
     });
 
